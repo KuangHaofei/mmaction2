c91ac354266af2f8c9fbdde8aebe40f975b2893f
KuangHaofei <haofeikuang@gmail.com>
2021-03-01 10:47:39 +0800

    fix bugs

--- tools/seco_hmdb_freeze_test.sh
+++ tools/seco_hmdb_freeze_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 python tools/test.py configs/recognition/tsn/tsn_r50_1x1x8_50e_hmdb51_seco-k400_rgb.py \
-    work_dirs/tsn_r50_1x1x8_50e_hmdb51_freeze_seco-k400_rgb/best_top1_acc_epoch_29.pth \
+    work_dirs/seco/tsn_r50_1x1x8_50e_hmdb51_freeze_seco-k400_rgb/best_top1_acc_epoch_29.pth \
     --eval top_k_accuracy mean_class_accuracy --out result.json
--- tools/seco_hmdb_train.sh
+++ tools/seco_hmdb_train.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./tools/dist_train.sh configs/recognition/tsn/seco/tsn_r50_1x1x8_50e_hmdb51_seco-k400_rgb.py 8 \
+./tools/dist_train.sh configs/recognition/tsn/tsn_r50_1x1x8_50e_hmdb51_seco-k400_rgb.py 8 \
   --work-dir work_dirs/seco/tsn_r50_1x1x8_50e_hmdb51_seco-k400_rgb --validate --seed 0 --deterministic
