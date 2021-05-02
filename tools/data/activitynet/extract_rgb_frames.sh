f4bc3ece2e1abc2d3472b762489bb83f9edac799
KuangHaofei <haofeikuang@gmail.com>
2021-05-02 22:52:29 +0800

    fix bugs

--- tools/data/activitynet/extract_rgb_frames.sh
+++ tools/data/activitynet/extract_rgb_frames.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-cd ../
-python build_rawframes.py ../../../drive2/ActivityNet/videos/ ../../../drive2/ActivityNet/rawframes/ --level 1 --ext mp4 --task rgb --new-short 256
-echo "Raw frames (RGB and tv-l1) Generated for train set"
-
-cd activitynet/
+python build_rawframes.py ~/drive2/ActivityNet/videos/ ~/drive2/ActivityNet/rawframes/ --level 1 --ext mp4 --task rgb --new-short 256
+python build_rawframes.py ~/drive2/ActivityNet/videos/ ~/drive2/ActivityNet/rawframes/ --level 1 --ext webm --task rgb --new-short 256
+echo "Raw frames (RGB) Generated for train set"
